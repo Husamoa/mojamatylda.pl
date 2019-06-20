9c0acd0a61366ee683a80b7d46caae5e45baa833
Husamoa <pawebial@gmail.com>
2019-06-20 15:29:20 +0200

    initial commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,40 @@
 import React from 'react'
-import { Container } from 'reactstrap'
-import Layout from '../components/layout'
+import Helmet from 'react-helmet'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import _get from 'lodash/get'
+import AlertTriangle from 'react-feather/dist/icons/alert-triangle'
 
-const NotFoundPage = () => (
-  <Container>
-    <Layout>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn't exist... the sadness.</p>
-    </Layout>
-  </Container>
+import Layout from '../components/Layout'
+
+export default ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query NotFoundPageQuery {
+        globalSettings: settingsYaml {
+          siteTitle
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <Helmet>
+          <title>404 – Page Not Found</title>
+        </Helmet>
+        <section className="section thick">
+          <div className="container skinny taCenter">
+            <p>
+              <AlertTriangle size="5rem" />
+            </p>
+            <h1>404 - Page Not Found</h1>
+            <p>
+              We can't find the page you are looking for!
+              <br />
+              Head back to{' '}
+              <Link to="/">{_get(data, 'globalSettings.siteTitle')}</Link>
+            </p>
+          </div>
+        </section>
+      </Layout>
+    )}
+  />
 )
-
-export default NotFoundPage
--- src/pages/about/about.md
+++
@@ -1,6 +0,0 @@
----
-contentType: about
-path: /about
-title: O mnie
----
-To jest przykładowa strona. Jestem mamą itd. Pisz co chcesz. Wystarczy, że zalogujesz się do [admin](https://www.mojamatylda.pl/admin/)
